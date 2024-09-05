2be8c9a4f6df0b8f71ee1e4d0168c7471d8a0182
if3ll <sunnwydays@gmail.com>
2024-09-05 11:42:36 -0400

    Implement menu and booking browsable APIs

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -1,8 +1,14 @@
-#update URLConf by including URL patterns of restaurant app
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from restaurant import views
+
+router = routers.DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
-   path('admin/', admin.site.urls),
-   path('restaurant/', include('restaurant.urls'))
+    path('admin/', admin.site.urls),
+    path('restaurant/', include('restaurant.urls')),
+    path('restaurant/menu/', include('restaurant.urls')),
+    path('restaurant/booking/', include(router.urls)),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets, permissions
+from .serializers import MenuSerializer, BookingSerializer
+from .models import Menu, Booking
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for viewing and editing Booking objects
+    """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
